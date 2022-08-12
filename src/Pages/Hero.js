bde83e04fbd5f60792e24b3ebf32910d3561093c
charliedavid214 <charliedavid214@gmail.com>
2022-08-12 05:27:44 -0700

    first commit

---
+++ src/Pages/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroTop from "./HeroTop";
+import HeroBottom from "./HeroBottom";
+
+const Hero = () => {
+  return (
+    <>
+      <HeroTop />
+      <HeroBottom />
+    </>
+  );
+};
+
+export default Hero;
